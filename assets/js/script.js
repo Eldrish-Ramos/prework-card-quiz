a61ff7773752bf7896bb1b4c45b394f62caec614
Eldrish <eldrish.ramosperez@gmail.com>
2025-01-16 18:10:15 -0800

    Fixed commiting array to local storage | Fixed logging a random card

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 let firstCardSet = [];
 
-const cardSet = {
+let cardSet = {
     term: "",
     description: "",
     question: "",
@@ -11,6 +11,7 @@ const cardSet = {
 
 function createCard(term, description, question, answer)
 {
+    cardSet = {};
     cardSet.term = term;
     cardSet.description = description;
     cardSet.question = question;
@@ -30,23 +31,25 @@ function getRandomCard(max) {
   }
 
 //const card1 = createCard("HTML", "Contains most of the informaiton about a webpage", "what does the body element represent", 0);
-const card1 = createCard("Card1", "This is card 1", "what does the body element represent", 0);
-const card2 = createCard("Card2", "This is card 2", "what does the body element represent", 0);
-const card3 = createCard("Card3", "This is card 3", "what does the body element represent", 0);
-const card4 = createCard("Card4", "This is card 4", "what does the body element represent", 0);
-const card5 = createCard("Card5", "This is card 5", "what does the body element represent", 0);
-const card6 = createCard("Card6", "This is card 6", "what does the body element represent", 0);
-const card7 = createCard("Card7", "This is card 7", "what does the body element represent", 0);
-const card8 = createCard("Card8", "This is card 8", "what does the body element represent", 0);
+let card1 = createCard("Card1", "This is card 1", "what does the body element represent", 0);
+let card2 = createCard("Card2", "This is card 2", "what does the body element represent", 0);
+let card3 = createCard("Card3", "This is card 3", "what does the body element represent", 0);
+let card4 = createCard("Card4", "This is card 4", "what does the body element represent", 0);
+let card5 = createCard("Card5", "This is card 5", "what does the body element represent", 0);
+let card6 = createCard("Card6", "This is card 6", "what does the body element represent", 0);
+let card7 = createCard("Card7", "This is card 7", "what does the body element represent", 0);
+let card8 = createCard("Card8", "This is card 8", "what does the body element represent", 0);
 
 storeToLocal(card1);
-// storeToLocal(card2);
-// storeToLocal(card3);
-// storeToLocal(card4);
-// storeToLocal(card5);
-// storeToLocal(card6);
-// storeToLocal(card7);
-// storeToLocal(card8);
+storeToLocal(card2);
+storeToLocal(card3);
+storeToLocal(card4);
+storeToLocal(card5);
+storeToLocal(card6);
+storeToLocal(card7);
+storeToLocal(card8);
 
+let randCard = getRandomCard(firstCardSet.length);
+console.log(firstCardSet[randCard]);
 
 
