25ddb2c67e5f7469c355913668998490f0146ba9
Eldrish <eldrish.ramosperez@gmail.com>
2025-01-16 19:33:48 -0800

    last commit for a while, added a function to randomize a given array

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,18 @@ function getRandomCard(max) {
     //return num;
   }
 
+function randomizeArray(arr)
+{
+    for (let i = arr.length - 1; i > 0; i--)
+    {
+        let j = Math.floor(Math.random() * (i + 1));
+        console.log('for loop i = :' + i);
+        console.log('random index j = :' + j);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
 //const card1 = createCard("HTML", "Contains most of the informaiton about a webpage", "what does the body element represent", 0);
 let card1 = createCard("Card1", "This is card 1", "what does the body element represent", 0);
 let card2 = createCard("Card2", "This is card 2", "what does the body element represent", 0);
@@ -51,7 +63,9 @@ storeToLocal(firstCardSet, card6);
 storeToLocal(firstCardSet, card7);
 storeToLocal(firstCardSet, card8);
 
-let randCard = getRandomCard(firstCardSet.length);
-console.log(firstCardSet[randCard]);
+//let randCard = getRandomCard(firstCardSet.length);
+//console.log(firstCardSet[randCard]);
+console.log(firstCardSet);
+console.log(randomizeArray(firstCardSet));
 
 
