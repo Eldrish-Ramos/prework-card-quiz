3c31405a0c2ec9d40a5a1e59ba0ebd20f3d6fca5
Eldrish <eldrish.ramosperez@gmail.com>
2025-01-19 13:35:04 -0800

    changed html to start looking more like a quiz page, added a static progress bar, removed code in .js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,58 @@
 // Test code for creating cards and displaying them
 
-let firstCardSet = [];
+let cardTitle = document.getElementById("#CARD_TERM");
 
-let cardSet = {
-    term: "",
-    description: "",
-    question: "",
-    answer: ""
-};
+// let firstCardSet = [];
 
-function createCard(term, description, question, answer)
-{
-    cardSet = {};
-    cardSet.term = term;
-    cardSet.description = description;
-    cardSet.question = question;
-    cardSet.answer = answer;
-    return cardSet;
-}
+// let cardSet = {
+//     term: "",
+//     description: "",
+//     question: "",
+//     answer: ""
+// };
 
-function storeToLocal(arrayToStore, object)
-{
-    arrayToStore.push(object);
-    localStorage.setItem('FlashCard', JSON.stringify(arrayToStore));
-}
+// function createCard(term, description, question, answer)
+// {
+//     cardSet = {};
+//     cardSet.term = term;
+//     cardSet.description = description;
+//     cardSet.question = question;
+//     cardSet.answer = answer;
+//     return cardSet;
+// };
+
+// function storeToLocal(arrayToStore, object)
+// {
+//     arrayToStore.push(object);
+//     localStorage.setItem('FlashCard', JSON.stringify(arrayToStore));
+// };
 
 
+// function getRandomCard(max) {
+//     return Math.floor(Math.random() * max);
+//     //return num;
+// };
 
-function getRandomCard(max) {
-    return Math.floor(Math.random() * max);
-    //return num;
-  }
+// //const card1 = createCard("HTML", "Contains most of the informaiton about a webpage", "what does the body element represent", 0);
+// let card1 = createCard("Card1", "This is card 1", "what does the body element represent", 0);
+// let card2 = createCard("Card2", "This is card 2", "what does the body element represent", 0);
+// let card3 = createCard("Card3", "This is card 3", "what does the body element represent", 0);
+// let card4 = createCard("Card4", "This is card 4", "what does the body element represent", 0);
+// let card5 = createCard("Card5", "This is card 5", "what does the body element represent", 0);
+// let card6 = createCard("Card6", "This is card 6", "what does the body element represent", 0);
+// let card7 = createCard("Card7", "This is card 7", "what does the body element represent", 0);
+// let card8 = createCard("Card8", "This is card 8", "what does the body element represent", 0);
 
-//const card1 = createCard("HTML", "Contains most of the informaiton about a webpage", "what does the body element represent", 0);
-let card1 = createCard("Card1", "This is card 1", "what does the body element represent", 0);
-let card2 = createCard("Card2", "This is card 2", "what does the body element represent", 0);
-let card3 = createCard("Card3", "This is card 3", "what does the body element represent", 0);
-let card4 = createCard("Card4", "This is card 4", "what does the body element represent", 0);
-let card5 = createCard("Card5", "This is card 5", "what does the body element represent", 0);
-let card6 = createCard("Card6", "This is card 6", "what does the body element represent", 0);
-let card7 = createCard("Card7", "This is card 7", "what does the body element represent", 0);
-let card8 = createCard("Card8", "This is card 8", "what does the body element represent", 0);
+// storeToLocal(firstCardSet, card1);
+// storeToLocal(firstCardSet, card2);
+// storeToLocal(firstCardSet, card3);
+// storeToLocal(firstCardSet, card4);
+// storeToLocal(firstCardSet, card5);
+// storeToLocal(firstCardSet, card6);
+// storeToLocal(firstCardSet, card7);
+// storeToLocal(firstCardSet, card8);
 
-storeToLocal(firstCardSet, card1);
-storeToLocal(firstCardSet, card2);
-storeToLocal(firstCardSet, card3);
-storeToLocal(firstCardSet, card4);
-storeToLocal(firstCardSet, card5);
-storeToLocal(firstCardSet, card6);
-storeToLocal(firstCardSet, card7);
-storeToLocal(firstCardSet, card8);
-
-let randCard = getRandomCard(firstCardSet.length);
-console.log(firstCardSet[randCard]);
+// let randCard = getRandomCard(firstCardSet.length);
+// console.log(firstCardSet[randCard]);
 
 
